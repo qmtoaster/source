561b1ad4ea6b3913f82a613c6f6f7be5cc9681da
Eric Broch <ebroch@whitehorsetc.com>
2024-04-12 00:47:39 -0600

    Create qdovauth.c

---
+++ qdovauth.c
@@ -0,0 +1,138 @@
+/*
+** Authenticate using dovecotadm.
+** This program reads from fd 3
+** username & password and auth-
+** enticates against dovecot.
+** Dovecot can be configured to
+** authenticate against most anything.
+**
+** Program:
+**    qdovauth
+**
+** Source
+**    qdovauth.c
+**
+** Build:
+**    gcc -o qdovauth qdovauth.c
+**
+** Use:
+**    Put in qmail submission/smtps
+**    supervise run script after
+**    qmail-smtpd to authenticate.
+**
+** Programmer:
+**    Eric C. Broch
+**    ebroch@whitehorsetc.com
+**
+** Date:
+**    04-10-2024
+**
+** Caveat:
+**    There is no license for this program.
+**    If your world collapses after you use
+**    this program, I am not to blame.
+**
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <syslog.h>
+
+#define CLEAN       0  // clean execution of parent and child
+#define CREDFILE    3  // Credentials file from qmail-smtpd
+#define DAUNAVAIL -10  // doveadm unavailable
+#define FRKFAIL   -11  // fork failed
+#define EXECFAIL  -12  // child executable failed
+#define CREDFAIL  -13  // Failed to read credentials from fd 3
+#define UPFAIL    -14  // Failed to read user
+#define PASSFAIL  -15  // Failed to read password
+#define CHALFAIL  -16  // Failed to read challenge
+
+#define AUTHSIZE 1000
+char buf[AUTHSIZE];
+char *user = NULL;
+char *pass = NULL;
+char *chal = NULL;
+char *doveadm = "/usr/bin/doveadm";
+
+void read_credentials();
+char *rjunk(char *text);
+
+int main( int argc, char *argv[] )
+{
+   char *success = "succeeded";
+   int status;
+   int fd[2];
+   int nbytes;
+   char  buff[1024];
+
+   char *alog = getenv("QDOVAUTH_LOG");
+   if ( alog ) openlog("qdovauth",LOG_PID,LOG_MAIL);
+   if ( access(doveadm,F_OK) ) {
+      if ( alog) syslog(LOG_NOTICE,"Dovecot's doveadm program does not exist");
+      exit(DAUNAVAIL);
+   }
+   read_credentials();
+   char *command[] = {doveadm,"auth","test","-a","/var/run/dovecot/auth-qmail",user,pass,NULL};
+
+   pipe(fd);
+   pid_t pid = fork();
+   if ( pid < 0 ) {
+      if ( alog ) syslog(LOG_NOTICE,"%s","Fork failed");
+      exit(FRKFAIL);
+   }
+   // Parent
+   else if (pid > 0) {
+      close(fd[1]);                           // Parent process closes up output side of pipe
+      memset(buff,'\0',sizeof(buff));
+      nbytes = read(fd[0],buff,sizeof(buff)); // Read output from child call to doveadm may eventually log/debug
+      if ( alog ) {
+         char * p = strstr(buff,"extra fields");
+         if ( p ) buff[p-buff-1]  = '\0';
+         p = getenv("TCPREMOTEIP");
+         if ( !p ) p = "UNKNOWN HOST";
+         syslog(LOG_NOTICE, "%s:IP:%s",rjunk(buff),p);
+      }
+      waitpid(pid, &status, 0);
+      if ( WIFEXITED(status) ) {
+         const int es = WEXITSTATUS(status);
+         if ( es ) exit(es);
+      }
+   }
+   // Child
+   else {
+      close(fd[0]);         // Child process closes up input side of pipe
+      dup2(fd[1],1);        // Send stdout from doveadm back to parent, we may want to do something with it, log perhaps.
+      dup2(fd[1],2);        // Send stderr from doveadm back to parent. we may want to do something with it, log perhaps.
+      execvp(command[0],command);
+      _exit(EXECFAIL);      // exec should never return
+   }
+   execvp(argv[1],argv+1);  // And now a simple way to kick off the next program
+   exit(CLEAN);             // Authentication succeeded
+}
+
+// Remove non-printable characters from text
+char *rjunk(char *text)
+{
+  for(char *new=text;*new!=0;++new) if ( *new < 32 || *new > 126 ) *new = ' ';
+  return(text);
+}
+// Read username and password from fd 3, qmail-smtpd
+void read_credentials()
+{
+
+  memset(buf,'\0',AUTHSIZE);
+  ssize_t bufsz = read(CREDFILE,buf,AUTHSIZE);
+  if ( bufsz <= 0 ) exit(CREDFAIL);
+  close(CREDFILE);
+
+  user=buf;
+  if ( strchr(user,'\0') ) pass=(strchr(user,'\0'))+1;
+  else exit(PASSFAIL);
+  if ( strchr(pass,'\0') ) chal=(strchr(pass,'\0'))+1;
+  else exit(CHALFAIL);
+  if ( strlen(user) <= 0 || strlen(pass) <= 0 ) exit(UPFAIL);
+
+}
